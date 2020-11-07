3daf0462d0c92c5cf6c7e756dd2c10023a58cec0
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-11-07 15:39:26 +0500

    frist commit

---
+++ functions/checkout/checkout.js
@@ -0,0 +1,19 @@
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+exports.handler = async event => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 8000,
+    currency: 'usd',
+    // Verify your integration in this guide by including this parameter
+    metadata: {customerId: "34343", orderId: "343434"},
+  });
+
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ client_secret: paymentIntent.client_secret }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
 import React from "react"
+import CheckoutForm from "../components/CheckoutForm"
 
+const  stripePromise = loadStripe("pk_test_51Hj5uqFoRvftLT4v1iVIeVed7WMNYbxIL4iflLHI6ytN1YjmF8YQ32P0imuL9iJb1q0iRHniYqyFpLB6QkYbgv3H00pSvexFVy")
 export default function Home() {
-  return <div>Hello world!</div>
+  return (<div>
+    
+    Hello world!
+    <Elements stripe={stripePromise}>
+
+    <CheckoutForm></CheckoutForm>
+    </Elements>
+    </div>)
+ 
+
 }
